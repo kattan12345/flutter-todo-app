f373c1d2fb981971179bae77a5f22dfdcaaf6fb1
katsutakeiichi <renoir5d@gmail.com>
2022-11-20 15:41:19 +0900

    Create page

--- practice/lib/main.dart
+++ practice/lib/main.dart
@@ -1,5 +1,29 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const Text('Practice', textDirection: TextDirection.ltr));
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}): super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}): super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Home'),
+      ),
+    );
+  }
 }
